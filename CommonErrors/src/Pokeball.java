d00961af9b5a7adf43bebaed3e22d21ec03abe73
BrendanSkunkworks <54401488+BrendanSkunkworks@users.noreply.github.com>
2019-08-27 08:04:03 -0400

    1st commit

---
+++ CommonErrors/src/Pokeball.java
@@ -0,0 +1,18 @@
+
+public class Pokeball {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("   @@@@@@@@@@");
+		System.out.println ("  @@Gamer@@@@@");
+		System.out.println (" @@@Nation@@@@@");
+		System.out.println ("@@@@@@@@@@@@@@@@");
+		System.out.println ("|||||[\"\"\"\"]|||||");
+		System.out.println ("|||||[....]|||||");
+		System.out.println ("oooooooooooooooo");
+		System.out.println (" oooooooooooooo");
+		System.out.println ("  oooooooooooo");
+		System.out.println ("   oooooooooo");
+	}
+
+}
